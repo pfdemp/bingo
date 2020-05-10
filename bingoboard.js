64608546442144ab557ea6845991b115b9009935
pfdemp <49198030+pfdemp@users.noreply.github.com>
2020-05-10 12:08:22 -0400

    Add files via upload

---
+++ bingoboard.js
@@ -0,0 +1,25 @@
+var colHead = new Array("B","I","N","G","O");
+
+function newGame() {
+	for(var asub=1; asub<76; asub++){
+		var cellID = "N" + asub;
+		document.getElementById(cellID).className = "numberoff";
+	}
+	document.getElementById("ballnum").innerHTML = "&nbsp;";
+}
+
+function getBall() {
+	var minrand = 1;
+	var maxrand = 75;
+	var numDupe = true;
+	while(numDupe){
+		var ballnum = Math.floor(Math.random() * (maxrand - minrand)) + minrand;
+		var ballID = "N" + ballnum;
+		if(document.getElementById(ballID).className == "numberoff"){
+			document.getElementById(ballID).className = "numberon";
+			ballLetter = colHead[Math.floor((ballnum-1)/15)];
+			document.getElementById("ballnum").innerHTML = ballLetter + ballnum;
+			numDupe = false;
+		}
+	}
+}
