1c1cc1a222df0030909e5d1abc0f9dc4a1ddef0e
pfdemp <49198030+pfdemp@users.noreply.github.com>
2020-05-12 16:34:25 -0400

    Add files via upload

--- bingoboard.js
+++ bingoboard.js
@@ -1,4 +1,5 @@
 var colHead = new Array("B","I","N","G","O");
+var balls = 0;
 
 function newGame() {
 	for(var asub=1; asub<76; asub++){
@@ -6,20 +7,28 @@ function newGame() {
 		document.getElementById(cellID).className = "numberoff";
 	}
 	document.getElementById("ballnum").innerHTML = "&nbsp;";
+	document.getElementById("message").innerHTML = " ";
+	balls = 0;
 }
 
 function getBall() {
-	var minrand = 1;
-	var maxrand = 76;
-	var numDupe = true;
-	while(numDupe){
-		var ballnum = Math.floor(Math.random() * (maxrand - minrand)) + minrand;
-		var ballID = "N" + ballnum;
-		if(document.getElementById(ballID).className == "numberoff"){
-			document.getElementById(ballID).className = "numberon";
-			ballLetter = colHead[Math.floor((ballnum-1)/15)];
-			document.getElementById("ballnum").innerHTML = ballLetter + ballnum;
-			numDupe = false;
+	if(balls > 74){
+		document.getElementById("message").innerHTML = "All balls have been selected.";
+	}
+	else {
+		var minrand = 1;
+		var maxrand = 76;
+		var numDupe = true;
+		while(numDupe){
+			var ballnum = Math.floor(Math.random() * (maxrand - minrand)) + minrand;
+			var ballID = "N" + ballnum;
+			if(document.getElementById(ballID).className == "numberoff"){
+				document.getElementById(ballID).className = "numberon";
+				ballLetter = colHead[Math.floor((ballnum-1)/15)];
+				document.getElementById("ballnum").innerHTML = ballLetter + ballnum;
+				numDupe = false;
+				balls++;
+			}	
 		}
 	}
 }
